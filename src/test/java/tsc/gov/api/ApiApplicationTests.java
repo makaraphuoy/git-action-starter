e4e6d3519847d8fa83173820e81239d739c8fb13
makara-tsc <makara.phuoy@techostartup.center>
2025-05-06 15:12:09 +0700

    fix: add simple test

--- src/test/java/tsc/gov/api/ApiApplicationTests.java
+++ src/test/java/tsc/gov/api/ApiApplicationTests.java
@@ -1,13 +1,28 @@
 package tsc.gov.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiApplicationTests {
+ @Autowired
+    private ApplicationContext applicationContext;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Basic check that the Spring context loads
+        assertThat(applicationContext).isNotNull();
+    }
+
+    @Test
+    void shouldHaveApiApplicationBean() {
+        // Replace "apiApplication" with any real bean name from your app
+        boolean hasBean = applicationContext.containsBeanDefinition("apiApplication");
+        assertThat(hasBean).isTrue();
+    }
 
 }
